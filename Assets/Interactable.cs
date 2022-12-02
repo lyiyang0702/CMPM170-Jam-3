f1e7a69d06884673406848546dd1daf809a5d36f
Yiyang Lu <lyiyang@ucsc.edu>
2022-12-01 23:51:45 -0800

    Revert "Update Interactable.cs"
    This reverts commit a10b86d1b90a279069fb520bb94c7984096fe9e3.

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -4,7 +4,7 @@ public class Interactable : MonoBehaviour
 {
     public virtual void Interact()
     {
-        // This method is meant to be override
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
